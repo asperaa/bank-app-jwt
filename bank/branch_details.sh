cd2b6597aba4d21f37dddd04f33f6e7377d3b2a1
Aditya Ankur Yadav <adityaankur44@gmail.com>
2019-07-13 22:54:59 +0530

    Update branch_details.sh

--- bank/branch_details.sh
+++ bank/branch_details.sh
@@ -1,6 +1,6 @@
 curl -X GET \
-  'http://localhost:8000/api/branch_detail/?limit=7&offset=4&name=YES%20BANK&city=NAGPUR' \
-  -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTYyNzU4Mjk1LCJqdGkiOiJmY2U3YWI0N2QyNzg0OWQ5OTc2ZWMwMGNiMmU1YmYyNCIsInVzZXJfaWQiOjF9.hOj8-MfE0TJ8P4OCgWBWdQ0wQyAlty5klQ4GU5WucoY' \
+  'http://banking-apps.herokuapp.com/api/branch_detail/?limit=7&offset=4&name=YES%20BANK&city=NAGPUR' \
+  -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTYzNDY5MzE4LCJqdGkiOiI2YWNmNzM3Yjc3YTc0NTVlYWU4YTQzMmFjM2ZmMzE2MyIsInVzZXJfaWQiOjF9.CJOjN3AFScxIgzdWdwe4e7ShIsI2KolGpSMLWsxr4hk' \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -H 'Postman-Token: 55c06a7b-7f4f-4b84-8b75-603edb96e06b' \
   -H 'cache-control: no-cache'
